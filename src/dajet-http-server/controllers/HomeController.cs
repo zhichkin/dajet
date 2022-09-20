1ef35f35474849ae0c8614d46012f44f8ad4821c
zhichkin <dima_zhichkin@mail.ru>
2022-09-21 02:36:32 +0300

    DaJet Studio UI - start of work

--- src/dajet-http-server/controllers/HomeController.cs
+++ src/dajet-http-server/controllers/HomeController.cs
@@ -39,5 +39,67 @@ namespace DaJet.Http.Controllers
                 Content = content
             };
         }
+        [HttpGet("ui/home/js")] public ContentResult LoadJavaScript()
+        {
+            string root = AppContext.BaseDirectory;
+            string filePath = Path.Combine(root, "pages", "home.js");
+
+            FileInfo info = new(filePath);
+
+            if (!info.Exists)
+            {
+                return new ContentResult()
+                {
+                    ContentType = "text/html",
+                    StatusCode = (int)HttpStatusCode.NotFound,
+                    Content = "<html><body>JavaScript is not found!</body></html>"
+                };
+            }
+
+            string content = string.Empty;
+
+            using (StreamReader reader = new(filePath, Encoding.UTF8))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            return new ContentResult()
+            {
+                ContentType = "text/javascript",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = content
+            };
+        }
+        [HttpGet("ui/home/css")] public ContentResult LoadStyleSheet()
+        {
+            string root = AppContext.BaseDirectory;
+            string filePath = Path.Combine(root, "pages", "home.css");
+
+            FileInfo info = new(filePath);
+
+            if (!info.Exists)
+            {
+                return new ContentResult()
+                {
+                    ContentType = "text/html",
+                    StatusCode = (int)HttpStatusCode.NotFound,
+                    Content = "<html><body>Style sheet is not found!</body></html>"
+                };
+            }
+
+            string content = string.Empty;
+
+            using (StreamReader reader = new(filePath, Encoding.UTF8))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            return new ContentResult()
+            {
+                ContentType = "text/css",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = content
+            };
+        }
     }
 }
---
+++ src/dajet-http-server/pages/home.js
@@ -0,0 +1,20 @@
+﻿function test() {
+
+    alert("Hello from test function!");
+}
+
+window.addEventListener("load", initTreeView, { once: true });
+
+function initTreeView() {
+
+    var toggler = document.getElementsByClassName("caret");
+
+    for (let i = 0; i < toggler.length; i++) {
+
+        toggler[i].addEventListener("click", function () {
+
+            this.parentElement.querySelector(".nested").classList.toggle("active");
+            this.classList.toggle("caret-down");
+        });
+    }
+}
