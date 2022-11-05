81da535a5503dd2367087f49e9810e0361c54bfb
zhichkin <dima_zhichkin@mail.ru>
2022-11-06 02:43:51 +0300

    new project: 1C data provider

---
+++ src/dajet-data-provider/OneDbConnection.cs
@@ -0,0 +1,112 @@
+﻿using DaJet.Metadata;
+using Microsoft.Data.SqlClient;
+using Npgsql;
+using System.Data;
+using System.Data.Common;
+using System.Transactions;
+using IsolationLevel = System.Data.IsolationLevel;
+
+namespace DaJet.Data.Provider
+{
+    public sealed class OneDbConnection : DbConnection
+    {
+        private readonly DbConnection _connection;
+
+        private readonly string IB_KEY;
+        private MetadataCache? _metadata;
+        private DatabaseProvider _provider;
+        private readonly string _connectionString;
+        public OneDbConnection(string? connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            _connectionString = connectionString;
+
+            if (connectionString.StartsWith("Host"))
+            {
+                _provider = DatabaseProvider.PostgreSql;
+                _connection = new NpgsqlConnection(connectionString);
+            }
+            else
+            {
+                _provider = DatabaseProvider.SqlServer;
+                _connection = new SqlConnection(connectionString);
+            }
+
+            //TODO: ib key generation algorithm !?
+            IB_KEY = _connection.Database;
+        }
+        private void InitializeMetadataCache()
+        {
+            if (MetadataSingleton.Instance.TryGetMetadataCache(IB_KEY, out _metadata, out string error))
+            {
+                return;
+            }
+
+            InfoBaseOptions options = new()
+            {
+                Key = IB_KEY,
+                DatabaseProvider = _provider,
+                ConnectionString = _connectionString
+            };
+            
+            MetadataSingleton.Instance.Add(options);
+
+            if (!MetadataSingleton.Instance.TryGetMetadataCache(IB_KEY, out _metadata, out error))
+            {
+                throw new InvalidOperationException($"Metadata cache error: {error}");
+            }
+        }
+        public override string ConnectionString
+        {
+            get { return _connection.ConnectionString; }
+            set { _connection.ConnectionString = value; }
+        }
+        public override string Database { get { return _connection.Database; } }
+        public override string DataSource { get { return _connection.DataSource; } }
+        public override string ServerVersion { get { return _connection.ServerVersion; } }
+        public override ConnectionState State { get { return _connection.State; } }
+        public override void ChangeDatabase(string databaseName)
+        {
+            _connection.ChangeDatabase(databaseName);
+        }
+        public override void Open()
+        {
+            _connection.Open();
+
+            InitializeMetadataCache();
+        }
+        public override void Close()
+        {
+            _connection.Close();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _connection.Dispose();
+            }
+
+            _metadata = null;
+        }
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        {
+            return _connection.BeginTransaction(isolationLevel);
+        }
+        public override void EnlistTransaction(Transaction? transaction)
+        {
+            _connection.EnlistTransaction(transaction);
+        }
+        //public override DataTable GetSchema()
+        //{
+        //    return base.GetSchema();
+        //}
+        protected override DbCommand CreateDbCommand()
+        {
+            return _connection.CreateCommand();
+        }
+    }
+}
---
+++ src/dajet-test/Test_OneDb_Provider.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace DaJet.Data.Provider.Test
+{
+    [TestClass] public class Test_OneDb_Provider
+    {
+        private const string PG_CONNECTION_STRING = "Host=127.0.0.1;Port=5432;Database=dajet-metadata-pg;Username=postgres;Password=postgres;";
+        private const string MS_CONNECTION_STRING = "Data Source=ZHICHKIN;Initial Catalog=dajet-metadata-ms;Integrated Security=True;Encrypt=False;";
+        [TestMethod] public void PG_Connection()
+        {
+            Db_Connection(PG_CONNECTION_STRING);
+        }
+        [TestMethod] public void MS_Connection()
+        {
+            Db_Connection(MS_CONNECTION_STRING);
+        }
+        private void Db_Connection(string connectionString)
+        {
+            OneDbConnection connection;
+
+            Stopwatch watch = new();
+            watch.Start();
+
+            using (connection = new(connectionString))
+            {
+                Console.WriteLine($"Database = {connection.Database}");
+                connection.Open();
+                Console.WriteLine($"Connection state = {connection.State}");
+            }
+            Console.WriteLine($"Connection state = {connection.State}");
+
+            watch.Stop();
+            Console.WriteLine($"Elapsed = {watch.ElapsedMilliseconds} ms");
+
+            Console.WriteLine();
+
+            watch.Reset();
+            watch.Start();
+
+            using (connection = new(connectionString))
+            {
+                Console.WriteLine($"Database = {connection.Database}");
+                connection.Open();
+                Console.WriteLine($"Connection state = {connection.State}");
+            }
+            Console.WriteLine($"Connection state = {connection.State}");
+
+            watch.Stop();
+            Console.WriteLine($"Elapsed = {watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
