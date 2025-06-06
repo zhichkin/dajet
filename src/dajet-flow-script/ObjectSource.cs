30997b8906f4ca9d83550f5944eb0dfe2f06d91b
zhichkin <dima_zhichkin@mail.ru>
2025-06-06 21:07:06 +0300

    DaJet 3.11.1
    1. DaJet Flow: DaJet Script Runner block
    2. DaJet Studio: dbview generator page
    3. DaJet Studio: md to db diagnostic page

--- src/dajet-flow-script/ObjectSource.cs
+++ src/dajet-flow-script/ObjectSource.cs
@@ -1,64 +1,62 @@
-﻿using DaJet.Data;
-using DaJet.Runtime;
-using System.Text;
+﻿//using DaJet.Data;
+//using DaJet.Runtime;
+//using System.Text;
 
 namespace DaJet.Flow.Script
 {
-    public sealed class ObjectSource : ISourceBlock, IOutputBlock<DataObject>
-    {
-        private IInputBlock<DataObject> _next;
-        public void LinkTo(in IInputBlock<DataObject> next)
-        {
-            _next = next;
-        }
-        private readonly IPipeline _pipeline;
-        private readonly IProcessor _processor;
-        private readonly ScriptOptions _options;
-        public ObjectSource(ScriptOptions options, IPipeline pipeline)
-        {
-            _options = options ?? throw new ArgumentNullException(nameof(options));
-            _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
+    //public sealed class ObjectSource : ISourceBlock, IOutputBlock<DataObject>
+    //{
+    //    private IInputBlock<DataObject> _next;
+    //    public void LinkTo(in IInputBlock<DataObject> next)
+    //    {
+    //        _next = next;
+    //    }
+    //    private readonly IPipeline _pipeline;
+    //    private readonly IProcessor _processor;
+    //    private readonly ScriptOptions _options;
+    //    public ObjectSource(ScriptOptions options, IPipeline pipeline)
+    //    {
+    //        _options = options ?? throw new ArgumentNullException(nameof(options));
+    //        _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
 
-            string script = Path.Combine(AppContext.BaseDirectory, _options.Script);
+    //        string script = Path.Combine(AppContext.BaseDirectory, _options.Script);
 
-            using (StreamReader reader = new(script, Encoding.UTF8))
-            {
-                script = reader.ReadToEnd();
-            }
+    //        using (StreamReader reader = new(script, Encoding.UTF8))
+    //        {
+    //            script = reader.ReadToEnd();
+    //        }
 
-            Dictionary<string, object> parameters = _options.Parameters is null ? new() : _options.Parameters;
+    //        Dictionary<string, object> parameters = _options.Parameters is null ? new() : _options.Parameters;
 
-            if (!StreamFactory.TryCreateStream(in script, in parameters, out _processor, out string error))
-            {
-                throw new Exception(error);
-            }
-        }
-        public void Execute()
-        {
-            _pipeline.UpdateMonitorStatus($"Executing: {_options.Script}");
+    //        if (!StreamFactory.TryCreateStream(in script, in parameters, out _processor, out string error))
+    //        {
+    //            throw new Exception(error);
+    //        }
+    //    }
+    //    public void Execute()
+    //    {
+    //        _pipeline.UpdateMonitorStatus($"Executing...");
 
-            // run once ?
+    //        DataObject output = null;
 
-            DataObject output = null;
+    //        _processor.Process();
 
-            _processor.Process();
+    //        if (_processor is RootProcessor root)
+    //        {
+    //            output = root.ReturnValue as DataObject;
+    //        }
 
-            if (_processor is RootProcessor root)
-            {
-                output = root.ReturnValue as DataObject;
-            }
+    //        _next?.Process(in output);
 
-            //DataObjectJsonConverter()
+    //        //DataObjectJsonConverter() ?
 
-            string message = output is null ? string.Empty : output.GetValue(0).ToString();
+    //        string message = output is null ? string.Empty : output.GetValue(0).ToString();
 
-            _pipeline.UpdateMonitorStatus($"Output: {message}");
-
-            _next?.Process(in output);
-        }
-        public void Dispose()
-        {
-            _processor.Dispose();
-        }
-    }
+    //        _pipeline.UpdateMonitorStatus($"Output: {message}");
+    //    }
+    //    public void Dispose()
+    //    {
+    //        _processor.Dispose();
+    //    }
+    //}
 }
---
+++ src/dajet-flow-script/Runner.cs
@@ -0,0 +1,52 @@
+﻿using DaJet.Runtime;
+using System.Text;
+
+namespace DaJet.Flow.Script
+{
+    public sealed class Runner : ISourceBlock
+    {
+        private readonly IPipeline _pipeline;
+        private readonly IProcessor _processor;
+        private readonly ScriptOptions _options;
+        public Runner(ScriptOptions options, IPipeline pipeline)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+            _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
+
+            string script = Path.Combine(AppContext.BaseDirectory, _options.Script);
+
+            using (StreamReader reader = new(script, Encoding.UTF8))
+            {
+                script = reader.ReadToEnd();
+            }
+
+            Dictionary<string, object> parameters = _options.Parameters is null ? new() : _options.Parameters;
+
+            if (!StreamFactory.TryCreateStream(in script, in parameters, out _processor, out string error))
+            {
+                throw new Exception(error);
+            }
+        }
+        public void Execute()
+        {
+            _pipeline.UpdateMonitorStatus("Executing...");
+
+            string message = string.Empty;
+
+            _processor.Process();
+
+            if (_processor is RootProcessor root && root.ReturnValue is not null)
+            {
+                message = root.ReturnValue.ToString();
+            }
+
+            _pipeline.UpdateMonitorStatus(message);
+        }
+        public void Dispose()
+        {
+            _processor.Dispose();
+            
+            _pipeline.UpdateMonitorStatus("Disposed");
+        }
+    }
+}
